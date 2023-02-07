593d143a5bf766dafc6691420696f840cc518314
emredurak01 <emredurak01@gmail.com>
2023-02-07 16:33:52 +0300

    Basic server

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+
+const http = require("http");
+const { Server } = require("socket.io");
+
+const server = http.createServer(app);
+const cors = require("cors");
+const PORT = 3001;
+
+app.use(cors());
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+
+server.listen(PORT, () => console.log(`Server is running `));
