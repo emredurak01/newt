2e213660f94a66386610e4dd1881e23e3dcc9746
emredurak01 <emredurak01@gmail.com>
2023-02-07 22:02:46 +0300

    Create room navigation

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { v4 as uuidv4 } from "uuid";
 
 function Home() {
+  const navigate = useNavigate();
+
+  const joinRoom = () => {
+    const roomID = uuidv4().slice(0, 6);
+    navigate({
+      pathname: `/room/${roomID}`,
+    });
+  };
+
   return (
-    <div>Home</div>
-  )
+    <div>
+      <button onClick={joinRoom}>Create room</button>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
